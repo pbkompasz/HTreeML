28bc8e017fd893ecd87b1cc4b38991548b5381de
Peter-Bence Kompasz <kompaszbence@gmail.com>
2021-08-03 20:48:21 +0300

    message

---
+++ src/algo.c
@@ -0,0 +1,6 @@
+
+
+char* fetch_website(char* url);
+char* parse_website(char* webpage);
+void* dfs(website* &website);
+void* bfs(website* &website);
---
+++ src/cli.h
@@ -0,0 +1,5 @@
+
+
+void print_help();
+void print_error(char* error);
+void print_result(char* result);
