e4d1e3ded238b711a447134378b067d8c5e6ed87
Peter-Bence Kompasz <kompaszbence@gmail.com>
2021-08-06 13:28:36 +0300

    getopt_long

--- src/cli.c
+++ src/cli.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <getopt.h>
 
 void print_help() {
 
@@ -35,15 +36,52 @@ Report bugs to cjwatson@debian.org.";
 
 void parse_args(int argc, char *argv[]) {
 	int opt;
-	enum { CHARACTER_MODE, WORD_MODE, LINE_MODE } mode = CHARACTER_MODE;
-	while ((opt = getopt(argc, argv, "lw")) != -1) {
-		switch (opt) {
-        	case 'l': printf("line mode"); break;
-        	case 'w': printf("word mode"); break;
-        	default:
-            		fprintf(stderr, "Usage: %s [-ilw] [file...]\n", argv[0]);
-            		exit(EXIT_FAILURE);
-        	}
-	}
+	int digit_optind = 0;
 	
+	static struct option long_options[] = {
+		{"add",     required_argument, 0,  0 },
+        	{"append",  no_argument,       0,  0 },
+                {"delete",  required_argument, 0,  0 },
+                {"verbose", no_argument,       0,  0 },
+                {"create",  required_argument, 0, 'c'},
+                {"file",    required_argument, 0,  0 },
+                {0,         0,                 0,  0 }
+	};
+	
+	while (1) {
+		int this_option_optind = optind ? optind : 1;
+		int option_index = 0;
+		char c = getopt_long(argc, argv, "abc", long_options, &option_index);
+		if (c == -1) break;
+		switch (c) {
+			case 0:
+                   		printf("option %s", long_options[option_index].name);
+                   		if (optarg)
+                       			printf(" with arg %s", optarg);
+                   		printf("\n");
+                   		break;
+			case 'a':
+                   		printf("option a\n");
+                   		break;
+
+			case 'b':
+                   		printf("option b\n");
+                   		break;
+
+               		case 'c':
+                   		printf("option c with value '%s'\n", optarg);
+                   		break;
+			default:
+                   		printf("?? getopt returned character code 0%o ??\n", c);
+               		
+		}
+	}
+	if (optind < argc) {
+               	printf("non-option ARGV-elements: ");
+               	while (optind < argc)
+			printf("%s ", argv[optind++]);
+               	printf("\n");
+     	}
+
+       	exit(EXIT_SUCCESS);	
 }
