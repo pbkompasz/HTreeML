71585b6bdb2ac1cfd9a97b5d49ccab9ff288e51b
Peter-Bence Kompasz <kompaszbence@gmail.com>
2021-08-08 17:06:59 +0300

    f

--- src/algo.c
+++ src/algo.c
@@ -1,33 +1,49 @@
 #include "algo.h"
 
+
+
 void run() {
-	printf("run asd");
-	CURL *curl;
-  	CURLcode res;
- 
-  	curl = curl_easy_init();
 }
 
-char* fetch_website(char* url) {
-	CURL *curl;
-  	CURLcode res;
- 
-  	curl = curl_easy_init();
-  	if(curl) {
-    		curl_easy_setopt(curl, CURLOPT_URL, url);
-    		res = curl_easy_perform(curl);
- 
-    		if(CURLE_OK == res) {
-      			char *ct;
-      			/* ask for the content-type */
-      			res = curl_easy_getinfo(curl, CURLINFO_CONTENT_TYPE, &ct);
- 
-      			if((CURLE_OK == res) && ct)
-        			printf("We received Content-Type: %s\n", ct);
-    		}
- 
-    		/* always cleanup */
-    		curl_easy_cleanup(curl);
-  	}
+static void add_transfer(CURLM *cm, int i) {
+	CURL *eh = curl_easy_init();
+	curl_easy_setopt(eh, CURLOPT_WRITEFUNCTION, write_cb);
+	curl_easy_setopt(eh, CURLOPT_URL, urls[i]);
+	curl_easy_setopt(eh, CURLOPT_PRIVATE, urls[i]);
+	curl_multi_add_handle(cm, eh);
+}
+
+char* fetch_websites(char* urls[]) {
+	// Number of urls to fetch
+	int num_urls = sizeof(urls) / sizeof(char *);
+
+	CURLM *cm;
+	CURLMsg *msg;
+	unsigned int transfers = 0;
+	int msgs_left = -1;
+	int still_alive = 1;
+
+	curl_global_init(CURL_GLOBAL_ALL);
+	cm = curl_multi_init();
+
+	// max connections limit
+	curl_multi_setopt(cm, CURLMOP_MAXCONNECTS, (long)MAX_PARALLEL);
+
+	for (transfers = 0; transfers < MAX_PARALLEL, transfers++) {
+		add_transfers(cm, transfers);
+	}
+
+	do {
+
+	} while (still_alive || (transfers < NUM_URLS))
+
+	
+
 	return 0;
 }
+
+struct website* parse_website(char* webpage) {
+	
+}
+
+
--- src/algo.h
+++ src/algo.h
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include <curl/curl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define MAX_PARALLEL 10
 
 struct website {
 	// Website address
