066866b897cdef8b0333498d1a43f08934c931c7
Peter-Bence Kompasz <kompaszbence@gmail.com>
2021-08-11 08:51:27 +0300

    Fetch site

--- src/algo.c
+++ src/algo.c
@@ -7,41 +7,34 @@ void run() {
 }
 
 char fetch_website(char* url) {
-		printf("hello");		
-
-
-	CURL *curl;
-	CURLcode res;
-	printf("hello");		
-
-
-	// Define custom headers
-	struct curl_slist *chunk = NULL;
-
-	char *user_agent = "user-agent: Mozilla/5.0 ";
-	//chunk = curl_slist_append(chunk, "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36");
-	chunk = curl_slist_append(chunk, user_agent);
-
-	// Set custom headers
-	curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
-/*	printf("hello");		
-
-	// set url
-	curl_easy_setopt(curl, CURLOPT_URL, url);
-	// make request
-	res = curl_easy_perform(curl);
-	// error
-	if (res != CURLE_OK) {
-		fprintf(stderr, "curl_easy_perform() failed %s\n", curl_easy_strerror(res)); 
-	}
-		printf("hello");		
-
-
-	// curl general cleanup
-	curl_easy_cleanup(curl);
-	// free custom headers
-	curl_slist_free_all(chunk);
-*/	return 0;
+	  CURL *curl;
+  CURLcode res;
+ 
+  curl = curl_easy_init();
+  if(curl) {
+    struct curl_slist *chunk = NULL;
+ 
+    chunk = curl_slist_append(chunk, "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36");
+ 
+    /* set our custom set of headers */
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
+ 
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+ 
+    res = curl_easy_perform(curl);
+    /* Check for errors */
+    if(res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
+ 
+    /* always cleanup */
+    curl_easy_cleanup(curl);
+ 
+    /* free the custom headers */
+    curl_slist_free_all(chunk);
+  }
+	return 0;
 }
 
 char* fetch_websites_chunk(char* urls[]) {
