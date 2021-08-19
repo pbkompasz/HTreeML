28b52239b673e123c575c6a6ab67d657dcc6c064
Peter-Bence Kompasz <kompaszbence@gmail.com>
2021-08-19 20:21:09 +0300

    Write into string

--- src/algo.c
+++ src/algo.c
@@ -5,12 +5,47 @@
 
 void run(struct control *cl) {
     printf("%s %i \n", cl->parent_url, cl->max_search_depth);    
+    fetch_website(cl->parent_url);
+}
+
+struct string {
+    char *ptr;
+    size_t len;
+};
+
+void init_string(struct string *s) {
+    s->len = 0;
+    s->ptr = malloc(s->len + 1);
+    if (s->ptr == NULL) {
+        fprintf(stderr, "malloc() failed\n");
+        exit(EXIT_FAILURE);
+    }
+    s->ptr[0] = '\0';
+}
+
+size_t writefunc(void *ptr, size_t size, size_t nmemb, struct string *s) {
+    size_t new_len = s->len + size*nmemb;
+    s->ptr = realloc(s->ptr, new_len + 1);
+    if (s->ptr == NULL) {
+        fprintf(stderr, "realloc() failed\n");
+        exit(EXIT_FAILURE);
+    }
+    memcpy(s->ptr + s->len, ptr, size*nmemb);
+    s->ptr[new_len] = '\0';
+    s->len = new_len;
+    
+    return size*nmemb;
 }
 
 char fetch_website(char* url) {
 	CURL *curl;
   	CURLcode res;
- 
+    
+    printf("Fetching site");
+    
+    struct string s;
+    init_string(&s);
+
   	curl = curl_easy_init();
     int this;
         printf("%i", 1+1);
@@ -21,6 +56,10 @@ char fetch_website(char* url) {
  
     	/* set our custom set of headers */
 	    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunc);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
 
         curl_easy_setopt(curl, CURLOPT_URL, url);
         curl_easy_setopt(curl, CURLOPT_VERBOSE, 0L);
@@ -31,6 +70,9 @@ char fetch_website(char* url) {
             fprintf(stderr, "curl_easy_perform() failed: %s\n",
                 curl_easy_strerror(res));
  
+        printf("DATA\n%s\nDATA\n", s.ptr);
+        free(s.ptr); 
+
         /* always cleanup */
         curl_easy_cleanup(curl);
  
